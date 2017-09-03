ab77f053004fb24dd05d06e201642c047edccdac
Peter Dalby <peterdalby93@gmail.com>
2017-09-03 20:03:45 +0200

    Registrerings pjat, mangler bare backend delen

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).on("submit", "form", function(e){
+$(document).on("submit", ".loginform", function(e){
 	e.preventDefault();
 		$.ajax ({
 			url: "./php/login.php",
@@ -19,3 +19,49 @@ $(document).on("submit", "form", function(e){
 		}
 		});
 });
+
+$(document).on("click", ".link", function(){
+					swal({
+					  title: "Registration",
+					  text: "Write a username.",
+					  type: "input",
+					  showCancelButton: true,
+					  closeOnConfirm: false,
+					  animation: "slide-from-top",
+					  inputPlaceholder: "Username"
+					},
+					function(sUsername){
+					  if (sUsername === false) return false;
+					  
+					  if (sUsername === "") {
+					    swal.showInputError("You need to write a username!");
+					    return false
+					  }
+					  
+					  else { //Not empty or false username, continue to password input.
+							swal({
+							  title: "Registration",
+							  text: "Write a password.",
+							  type: "input",
+							  inputType: "password",
+							  showCancelButton: true,
+							  closeOnConfirm: false,
+							  animation: "slide-from-top",
+							  inputPlaceholder: "Password"
+							},
+							function(sPassword){
+								  if (sPassword === false) return false;
+								  
+								  if (sPassword === "") {
+								    swal.showInputError("You need to write a password!");
+								    return false
+								  }
+								  else {
+								  	swal("POST", ""+sUsername+" & "+sPassword, "success");
+								  }
+							});
+							  	  
+						 }
+					});
+})
+
